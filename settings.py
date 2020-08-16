f928f9bd46572e991455c2de4a8b9979979cb40c
Denea <dprodan289@gmail.com>
2020-08-16 14:54:19 +0300

    minor bug fix

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@ class Settings:
     and (COLS + 1) * 2)"""
     screen_width = 560
     screen_height = 560
-    ROWS, COLS = 8, 8
-    COEFICIENT_X = screen_width / (ROWS * 15 + (ROWS + 1) * 2)
-    COEFICIENT_Y = screen_height / (COLS * 15 + (COLS + 1) * 2)
+    ROWS, COLS = 4, 8
+    COEFICIENT_X = screen_width / (COLS * 15 + (COLS + 1) * 2)
+    COEFICIENT_Y = screen_height / (ROWS * 15 + (ROWS + 1) * 2)
     image_width = round(15 * COEFICIENT_X)
     image_height = round(15 * COEFICIENT_Y)
     boarder_width = round(2 * COEFICIENT_X)
